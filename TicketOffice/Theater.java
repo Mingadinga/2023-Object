60512e56709d656be885a2d84c8688b583b4ed9f
mingadinga <minpearl0826@gmail.com>
2023-02-20 16:23:53 +0900

    chap01 - step1 절차지향적인 코드

---
+++ TicketOffice/Theater.java
@@ -0,0 +1,31 @@
+public class Theater {
+    private TicketSeller ticketSeller;
+
+    public Theater(TicketSeller ticketSeller) {
+        this.ticketSeller = ticketSeller;
+    }
+
+    public void enter(Audience audience) {
+        if (audience.getBag().hasInvitation()) {
+            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
+            audience.getBag().setTicket(ticket);
+        } else {
+            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
+            audience.getBag().minusAmount(ticket.getFee());
+            ticketSeller.getTicketOffice().plusAmount(ticket.getFee());
+            audience.getBag().setTicket(ticket);
+        }
+    }
+
+    public static void main(String[] args) {
+        Long amount = 10L;
+        TicketOffice ticketOffice = new TicketOffice(10L, new Ticket(), new Ticket(), new Ticket());
+        TicketSeller ticketSeller = new TicketSeller(ticketOffice);
+        Theater theater = new Theater(ticketSeller);
+
+        Bag bag1 = new Bag(5L);
+        Audience audience1 = new Audience(bag1);
+
+        theater.enter(audience1);
+    }
+}
---
+++ TicketOffice/Ticket.java
@@ -0,0 +1,11 @@
+public class Ticket {
+    private Long fee;
+
+    public Ticket() {
+        this.fee = 5L;
+    }
+
+    public Long getFee() {
+        return fee;
+    }
+}
